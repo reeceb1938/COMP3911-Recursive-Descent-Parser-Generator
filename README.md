de288bce238a30892145f0bbeb2a30c942340fda
reece <sc17rjb@leeds.ac.uk>
2021-02-12 13:53:40 +0000

    Added logging library spdlog

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # COMP3931-Recursive-Descent-Parser-Generator
 
+## References & Licences
+
+References for external content
+
+| Resource | Usage | Licence |
+| - | - | - |
+| .gitignore templates | .gitignore | [CC 1.0 Universal](https://github.com/github/gitignore/blob/master/LICENSE) |
+| spdlog | Logging library | [MIT](https://github.com/gabime/spdlog/blob/v1.x/LICENSE) |
---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "spdlog/spdlog.h"
+
+int main(int argc, char const* argv[]) {
+    // spdlog::info("Welcome to spdlog version {}.{}.{}  !", SPDLOG_VER_MAJOR, SPDLOG_VER_MINOR, SPDLOG_VER_PATCH);
+
+    spdlog::warn("Easy padding in numbers like {:08d}", 12);
+    spdlog::critical("Support for int: {0:d};  hex: {0:x};  oct: {0:o}; bin: {0:b}", 42);
+    spdlog::info("Support for floats {:03.2f}", 1.23456);
+    spdlog::info("Positional args are {1} {0}..", "too", "supported");
+    spdlog::info("{:>8} aligned, {:<8} aligned", "right", "left");
+
+    return 0;
+}
